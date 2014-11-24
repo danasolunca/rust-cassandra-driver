fa0b5ee72825e15cbc9a8207e48781eeab5a850b
Jon Haddad <jon@jonhaddad.com>
2014-11-24 14:48:30 -0800

    fixed typo breaking tests

--- src/types.rs
+++ src/types.rs
@@ -233,7 +233,7 @@ impl CassValue {
     #[test]
     fn string_wrapping() {
       let test_string = "test_string2345678".to_string();
-      let cass_string:cassandra_internal_api::CassString = super::CassValue::str_to_cass_string(&test_string);
+      let cass_string:cass_internal_api::CassString = super::CassValue::str_to_cass_string(&test_string);
       //println!("cassstr: {}", cass_string);
       let reconstituted:String = super::CassValue::cass_string_to_str(cass_string);
       println!("reconstituted: {}", reconstituted);
